7e70e48652b89d991786a62617c1c7712a93239d
Mardarovsky <mardarovsky@gmail.com>
2021-10-21 20:46:26 +0300

    1. base ready

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,19 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  const d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    return [];
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr; // array
 }
 
@@ -8,6 +21,22 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
   // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`
+  } else if (isNaN(contribution)) {
+    return `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`
+  } else if (isNaN(amount)) {
+    return `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`
+  }
 
-  return totalAmount;
+  const month = new Date().getMonth();
+  const year = new Date().getFullYear();
+  const numberOFyears = date.getFullYear() - year;
+  const n = numberOFyears * 12 - month + date.getMonth();
+  const P = (percent / 100) / 12;
+  const S = amount - contribution;
+  const payment = S * (P + (P / (((1 + P) ** n) - 1)))
+  totalAmount = (payment * n).toFixed(2);
+
+  return Number(totalAmount);
 }
