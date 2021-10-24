1c59fd6d8696c26fa36f6ddf804b6939d05754e2
Mardarovsky <mardarovsky@gmail.com>
2021-10-24 11:40:31 +0300

    Arrays ready

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,8 +1,13 @@
+'use strict';
+
 function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
-
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  arr1.every((value, i) => value === arr2[i] ? result = true : result = false);
   return result; // boolean
 }
 
@@ -10,6 +15,6 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
   return resultArr; // array
 }
