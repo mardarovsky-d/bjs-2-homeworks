20cb420ce63487620977faa7a57155fd75a17017
Mardarovsky <mardarovsky@gmail.com>
2021-10-25 23:43:39 +0300

    Classes main task is done

---
+++ 5.classes/task.js
@@ -0,0 +1,82 @@
+'use strict';
+
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+    }
+
+    type = null;
+
+    fix() {
+        this.state *= 1.5;
+    }
+
+    set state(num) {
+        if (num >= 100) {
+            num = 100;
+        } else if (num <= 0) {
+            num = 0;
+        }
+        this._state = num;
+    }
+
+    get state() {
+        if (this._state === undefined) {
+            this._state = 100;
+        }
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    type = 'magazine';
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+    }
+    type = 'book';
+}
+
+class NovelBook extends Book {
+    type = 'novel';
+}
+
+class FantasticBook extends Book {
+    type = 'fantastic';
+}
+
+class DetectiveBook extends Book {
+    type = 'detective';
+}
+
+class Library {
+    constructor(name) {
+        this.name = name;
+    }
+    books = [];
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        let searchingBook;
+        this.books.forEach(book => book[type] === value ? searchingBook = book : searchingBook = null);
+        return searchingBook;
+    }
+
+    giveBookByName(bookName) {
+        let givingBook = null;
+        let newBookArray = [];
+        this.books.forEach(book => book.name === bookName ? givingBook = book : newBookArray.push(book));
+        this.books = newBookArray;
+        return givingBook;
+    }
+}
