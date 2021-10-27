01f15e241c212014074e737e6460993677a1325c
Mardarovsky <mardarovsky@gmail.com>
2021-10-27 12:22:49 +0300

    Task completed

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function parseCount(value) {
+    let result = Number.parseInt(value);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        parseCount(value);
+    } catch (err) {
+        return err;
+    }
+    return parseCount(value);
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+        if (this.a + this.b < this.c || this.a + this.c < this.b || this.c + this.b < this.a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+    }
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+    getArea() {
+        const p = (this.a + this.b + this.c) / 2;
+        const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch {
+        return {
+            getPerimeter: () => "Ошибка! Треугольник не существует",
+            getArea: () => "Ошибка! Треугольник не существует"
+        }
+    }
+}
